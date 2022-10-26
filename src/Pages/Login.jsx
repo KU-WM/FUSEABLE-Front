c494852da844da0ea4d007831503b87968dbef56
KU-WM <112204749+KU-WM@users.noreply.github.com>
2022-10-26 23:54:16 +0900

    logo

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../css/Login.css';
+import {ReactComponent as Logo } from '../images/fuseable.svg';
+import Logo_image from '../images/Logo_image.png';
+
+
+function Login () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Logo className='logo'/>
+        </div>
+        <div className="test">
+          <a href={process.env.REACT_APP_LoginURL}>
+            <img
+          src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+          width="222"
+          alt="카카오 로그인 버튼"
+            />
+          </a>
+          <a href={process.env.REACT_APP_LogoutURL} id="logout-btn">Logout</a>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+
+export default Login;
